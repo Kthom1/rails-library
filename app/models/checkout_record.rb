87be3dccfeb0924cc2568a8d06069e09a2c03c3c
Kthom1 <keenan.thompson67@gmail.com>
2020-06-13 16:04:09 +0800

    Created checkout record model

---
+++ app/models/checkout_record.rb
@@ -0,0 +1,2 @@
+class CheckoutRecord < ApplicationRecord
+end
---
+++ db/migrate/20200613080131_create_checkout_records.rb
@@ -0,0 +1,8 @@
+class CreateCheckoutRecords < ActiveRecord::Migration[5.2]
+  def change
+    create_table :checkout_records do |t|
+
+      t.timestamps
+    end
+  end
+end
