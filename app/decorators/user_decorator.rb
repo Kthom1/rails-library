afeeebe0b0a3a89083f3c4409203ad9b8675815a
Kthom1 <keenan.thompson67@gmail.com>
2020-06-14 03:55:13 +0800

    Added full_name method to user decorator

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,5 +1,11 @@
+# frozen_string_literal: true
+
 class UserDecorator < Draper::Decorator
   delegate_all
+
+  def full_name
+    "#{object.first_name} #{object.last_name}"
+  end
 
   # Define presentation-specific methods here. Helpers are accessed through
   # `helpers` (aka `h`). You can override attributes, for example:
@@ -9,5 +15,4 @@ class UserDecorator < Draper::Decorator
   #       object.created_at.strftime("%a %m/%d/%y")
   #     end
   #   end
-
 end
