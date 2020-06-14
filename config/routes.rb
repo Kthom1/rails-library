41f686026a018513446d4e0e0f9d6b5af169b99e
Kthom1 <keenan.thompson67@gmail.com>
2020-06-14 14:55:12 +0800

    Added home controller; Set root to home#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root to: 'home#index'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, controllers: {
     registrations: 'members/registrations'
