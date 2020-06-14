9410da8b419d52b5fb99e5c1b63ea564a704e377
Kthom1 <keenan.thompson67@gmail.com>
2020-06-14 18:06:42 +0800

    Member redirected to edit page after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :authenticate_user!
+
+  def after_sign_in_path_for(_resource)
+    return edit_user_registration_path if current_user.member?
+
+    root_path
+  end
 end
