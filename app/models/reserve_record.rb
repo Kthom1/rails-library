d20007afe7da55f5e9c8b80e2abe2b27d53ad6eb
Kthom1 <keenan.thompson67@gmail.com>
2020-06-14 14:37:12 +0800

    Added foreign key to Reserve and Checkout Records

--- app/models/reserve_record.rb
+++ app/models/reserve_record.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ReserveRecord < ApplicationRecord
-  belongs_to :member, class_name: 'Member'
-  belongs_to :staff, class_name: 'Staff'
+  belongs_to :member, class_name: 'Member', foreign_key: 'member_id'
+  belongs_to :staff, class_name: 'Staff', foreign_key: 'staff_id'
 end
