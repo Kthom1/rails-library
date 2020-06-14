45d468c4a89d3e565850697a3529d28ff22e75f9
Kthom1 <keenan.thompson67@gmail.com>
2020-06-15 02:07:15 +0800

    Removed type from update permissions for user

--- app/controllers/members/registrations_controller.rb
+++ app/controllers/members/registrations_controller.rb
@@ -16,6 +16,6 @@ class Members::RegistrationsController < Devise::RegistrationsController
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name type])
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name type])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name])
   end
 end
