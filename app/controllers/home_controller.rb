2688eb89446658e45c62ab17802cacab1de8cbdd
Kthom1 <keenan.thompson67@gmail.com>
2020-06-15 04:48:33 +0800

    Added has many books through to member

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,5 +4,6 @@ class HomeController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
   def index
     @books = Book.limit(5).order('id desc')
+    @checked_out_books = current_user.checked_out_books
   end
 end
--- app/models/member.rb
+++ app/models/member.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Member < User
-  has_many :reserved_books, class_name: 'ReserveRecord', foreign_key: 'member_id'
-  has_many :checked_out_books, class_name: 'CheckoutRecord', foreign_key: 'member_id'
+  has_many :reserve_records, class_name: 'ReserveRecord', foreign_key: 'member_id'
+  has_many :checkout_records, class_name: 'CheckoutRecord', foreign_key: 'member_id'
+  has_many :books, through: :checkout_records
+  has_many :books, through: :reserve_records
 end
